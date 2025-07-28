cff96d68ec80c6d33ce4600f6c1c788b86de992e
Henrique Viana Abade <henrique.abade@santissimodecor.com.br>
2025-07-28 13:50:42 -0300

    Add Contatos module and update menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -154,6 +154,10 @@ document.querySelectorAll('.sidebar-item[data-page], .submenu-item[data-page]').
         if (page === 'pedidos') {
             loadPage('pedidos');
         }
+        // Carrega o módulo de Contatos ao clicar no item correspondente
+        if (page === 'contatos') {
+            loadPage('contatos');
+        }
     });
 });
 
